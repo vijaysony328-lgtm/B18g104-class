0e21807dca4d923b9b527cfeff2baed1f6f02059
vijaysony328-lgtm <vijaysony328@gmail.com>
2026-06-19 15:20:33 +0530

    Delete st.tf

--- st.tf
+++
@@ -1,9 +0,0 @@
-
-
-resource "azurerm_storage_account" "st" {
-    name                     = "stblock"
-    resource_group_name     ="rg_block"
-    location                 = "westus"
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
